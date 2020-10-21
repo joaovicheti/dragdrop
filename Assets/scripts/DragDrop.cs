32937697916d5d082f3f0c4719e9473c3111cced
João <joaovicheti2809@gmail.com>
2020-10-21 16:10:29 -0300

    codigo do jogo

---
+++ Assets/scripts/DragDrop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IEndDragHandler {
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnMouseDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
