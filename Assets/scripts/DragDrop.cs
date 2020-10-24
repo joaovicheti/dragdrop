7c6018b85af8bb77ab4fa9cf8a6f30bb8ca5b6a0
João <joaovicheti2809@gmail.com>
2020-10-24 13:24:44 -0300

    novo codigo

--- Assets/scripts/DragDrop.cs
+++ Assets/scripts/DragDrop.cs
@@ -1,30 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IEndDragHandler {
+public class DragDrop : MonoBehaviour
+{
+    public GameObject test1;
+    private bool moving;
+    private bool finish;
 
-    private RectTransform rectTransform;
+    private float testx;
+    private float testy;
 
-    private void Awake()
+    private Vector3 resetposition;
+    void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
+        resetposition = this.transform.localPosition;
     }
 
-    public void OnMouseDrag(PointerEventData eventData)
+    void Update()
     {
-        rectTransform.anchoredPosition += eventData.delta;
+        if (finish == false)
+        {
+            if (moving)
+            {
+                Vector3 mousepos;
+                mousepos = Input.mousePosition;
+                mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+
+                this.gameObject.transform.localPosition = new Vector3(mousepos.x - testx, mousepos.y - testy, this.gameObject.transform.localPosition.z);
+            }
+        }
 
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnEndDrag");
-    }
 
-    public void OnPointerDown(PointerEventData eventData)
+    private void OnMouseDown()
     {
-        throw new System.NotImplementedException();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousepos;
+            mousepos = Input.mousePosition;
+            mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+
+            testx = mousepos.x - this.transform.localPosition.x;
+
+            testy = mousepos.y - this.transform.localPosition.y;
+
+            moving = true;
+        }
+
+    }
+    private void OnMouseUp()
+    {
+        moving = false;
+        if (Mathf.Abs(this.transform.localPosition.x - test1.transform.localPosition.x) <= 0.5f &&
+            Mathf.Abs(this.transform.localPosition.y - test1.transform.localPosition.y) <= 0.5f)
+
+        {
+            this.transform.position = new Vector3(test1.transform.position.x, test1.transform.position.y, test1.transform.position.z);
+            finish = true;
+            GameObject.Find("ganhou").GetComponent<wins>().addpoint();
+        }
+        else
+        {
+            this.transform.localPosition = new Vector3(resetposition.x, resetposition.y, resetposition.z);
+        }
     }
 }
---
+++ Assets/scripts/wins.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wins : MonoBehaviour
+{
+    private int pointowin;
+    private int currentpoints;
+    public GameObject mysword;
+    void Start()
+    {
+        pointowin = mysword.transform.childCount;
+    }
+
+    void Update()
+    {
+        if (currentpoints >= pointowin)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void addpoint()
+    {
+        currentpoints++;
+    }
+}
