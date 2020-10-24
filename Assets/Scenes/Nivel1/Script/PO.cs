d6d6bc95760fe0a85cde7399ed534273728d7f2b
Elton Naoki Iwakura <eltoniwakura@tce.ms.gov.br>
2020-10-24 16:32:50 -0400

    implemtacao do prototipo

---
+++ Assets/Scenes/Nivel1/Script/PO.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PO : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rextTransform;
+    private CanvasGroup canvasGroup;
+    
+    private void Awake()
+    {
+        rextTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rextTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+}
---
+++ Assets/Scenes/Nivel1/Script/ProximoNivel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProximoNivel : MonoBehaviour
+{
+    public Button SA;
+    public Button PO;
+    public GameObject Proximo;
+    void Start()
+    {
+        Proximo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SA.colors.normalColor == Color.green && PO.colors.normalColor == Color.green)
+        {
+            Proximo.SetActive(true);
+        }
+    }
+}
